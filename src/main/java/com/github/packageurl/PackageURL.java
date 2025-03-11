62ac90975ef802236a68276e0b46d9cada3b8688
David Walluck <5445917+dwalluck@users.noreply.github.com>
2025-03-11 06:10:00 -0400

    fix: correct issues with `PackageURL` (#159)
    * Fix potential null pointers
    * Use `String::replace()` instead of `replaceAll` for single characters
    * Use `isEmpty` which can be more efficient
    * Remove exception which is not thrown

--- src/main/java/com/github/packageurl/PackageURL.java
+++ src/main/java/com/github/packageurl/PackageURL.java
@@ -344,9 +344,6 @@ public final class PackageURL implements Serializable {
     }
 
     private String validateVersion(final String value) {
-        if (value == null) {
-            return null;
-        }
         return value;
     }
 
@@ -445,9 +442,9 @@ public final class PackageURL implements Serializable {
             purl.append("@").append(percentEncode(version));
         }
         if (! coordinatesOnly) {
-            if (qualifiers != null && qualifiers.size() > 0) {
+            if (qualifiers != null && !qualifiers.isEmpty()) {
                 purl.append("?");
-                qualifiers.entrySet().stream().forEachOrdered((entry) -> {
+                qualifiers.entrySet().stream().forEachOrdered(entry -> {
                     purl.append(toLowerCase(entry.getKey()));
                     purl.append("=");
                     purl.append(percentEncode(entry.getValue()));
@@ -473,7 +470,7 @@ public final class PackageURL implements Serializable {
     }
 
     private static String uriEncode(String source, Charset charset) {
-        if (source == null || source.length() == 0) {
+        if (source == null || source.isEmpty()) {
             return source;
         }
 
@@ -710,7 +707,7 @@ public final class PackageURL implements Serializable {
     private Map<String, String> parseQualifiers(final String encodedString) throws MalformedPackageURLException {
         try {
             final TreeMap<String, String> results = Arrays.stream(encodedString.split("&"))
-                    .collect(TreeMap<String, String>::new,
+                    .collect(TreeMap::new,
                             (map, value) -> {
                                 final String[] entry = value.split("=", 2);
                                 if (entry.length == 2 && !entry[1].isEmpty()) {
@@ -720,7 +717,7 @@ public final class PackageURL implements Serializable {
                                     }
                                 }
                             },
-                            TreeMap<String, String>::putAll);
+                            TreeMap::putAll);
             return validateQualifiers(results);
         } catch (ValidationException e) {
             throw new MalformedPackageURLException(e);
@@ -728,7 +725,7 @@ public final class PackageURL implements Serializable {
     }
 
     @SuppressWarnings("StringSplitter")//reason: surprising behavior is okay in this case
-    private String[] parsePath(final String value, final boolean isSubpath) throws MalformedPackageURLException {
+    private String[] parsePath(final String value, final boolean isSubpath) {
         if (value == null || value.isEmpty()) {
             return null;
         }
