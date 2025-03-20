8fa5b1a89a93059da6a7dd6a93afb28f760c30a0
David Walluck <dwalluck@redhat.com>
2025-03-20 16:11:13 -0400

    Inline second argument of `indexOfFirstPercentChar`

--- src/main/java/com/github/packageurl/PackageURL.java
+++ src/main/java/com/github/packageurl/PackageURL.java
@@ -589,8 +589,8 @@ public final class PackageURL implements Serializable {
         return new String(chars);
     }
 
-    private static int indexOfPercentChar(final byte[] bytes, final int start) {
-        return IntStream.range(start, bytes.length)
+    private static int indexOfFirstPercentChar(final byte[] bytes) {
+        return IntStream.range(0, bytes.length)
                 .filter(i -> isPercent(bytes[i]))
                 .findFirst()
                 .orElse(-1);
@@ -629,7 +629,7 @@ public final class PackageURL implements Serializable {
         }
 
         byte[] bytes = source.getBytes(StandardCharsets.UTF_8);
-        int i = indexOfPercentChar(bytes, 0);
+        int i = indexOfFirstPercentChar(bytes);
 
         if (i == -1) {
             return source;
